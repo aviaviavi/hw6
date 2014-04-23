01a69f1fcdd68f4f9b0daa3014143365a8e0d3df
Justin Kay <justinkay92@gmail.com>
2014-04-22 18:33:01 -0700

    multilayer?

--- make_predictions.m
+++ make_predictions.m
@@ -1,12 +1,15 @@
 function [ predicted ] = make_predictions( w, obs )
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
+    predicted = {obs};
     i = 0;
     if length(w) ~= 1
+        % hidden layers
         for i = 1:length(w) - 1
-         %obs = tanh(obs * w{i});
+            predicted{i+1} = tanh(predicted{i} * w{i});
         end
     end
-    predicted = sigmoid(obs * w{i+1});
+    % output layer
+    predicted{i+2} = sigmoid(predicted{i+1} * w{i+1});
 end
 
